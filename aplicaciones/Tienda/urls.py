83fdc38f0395a2a538846313135e9413d35cc03b
Ryuzakiii-cl <ch.lhoest@duocuc.cl>
2023-07-10 21:12:28 -0400

    se modifica plantilla base para navegar en navbar

--- aplicaciones/Tienda/urls.py
+++ aplicaciones/Tienda/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.cargarInicio),
-    path('',views.productos)
+    path('',views.cargarInicio, name='inicio'),
+    path('productos/',views.productos, name='productos'),
+    path('carrito/',views.carrito, name='carrito'),
+    path('cuidados/',views.cuidados, name='cuidados'),
+    path('login/',views.login, name='login'),
+    path('registro/',views.registro, name='registro'),
+    path('veterinaria/',views.veterinaria, name='veterinaria')
 ]
+
