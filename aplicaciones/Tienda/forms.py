2f7e0e51b4c741a43b266b8b46e8ebc42f4f7928
Ryuzakiii-cl <ch.lhoest@duocuc.cl>
2023-07-13 21:45:40 -0400

    se intento agregar crispy form pero no funciono

--- aplicaciones/Tienda/forms.py
+++ aplicaciones/Tienda/forms.py
@@ -1,5 +1,20 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+
+class MyForm(forms.Form):
+    # Campos de tu formulario
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            # Definición de la estructura de tu formulario
+            Submit('submit', 'Enviar')
+        )
 
 class CustomUserCreationForm(UserCreationForm):
     pass
